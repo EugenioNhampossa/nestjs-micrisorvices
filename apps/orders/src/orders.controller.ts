ec04440dcb041fcde65ac1bec989e68250fd645c
EugenioNhampossa <eugenioanhampossa@gmail.com>
2024-10-12 15:10:36 +0200

    chore: add billing microservice

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-orders.dto';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @Post()
+  create(@Body() dto: CreateOrderDto) {
+    return this.ordersService.create(dto);
+  }
+
   @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
+  findAll() {
+    return this.ordersService.findAll();
   }
 }
--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-orders.dto';
+import { OrdersRepository } from './orders.repository';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly ordersRepository: OrdersRepository) {}
+
+  async create(dto: CreateOrderDto) {
+    return this.ordersRepository.create(dto);
+  }
+
+  async findAll() {
+    return this.ordersRepository.find({});
   }
 }
